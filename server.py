0d6b3367bba3f02e39b345a0e20e42963d93f136
anie1337 <dison2233@126.com>
2022-05-30 13:32:04 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,27 @@
+import asyncio
+from websocket import create_connection
+from websockets import serve
+import json
+
+address: str = '121.40.246.155'
+port: str = '27015'
+verifykey: str = '1234567890'
+miraibotqq: str = '3031718988'
+url = 'ws://{}:{}/all?verifyKey={}&qq={}'.format(address, port, verifykey, miraibotqq)
+
+
+ws = create_connection(url)
+sessionkey = json.loads(ws.recv())['data']['session']
+
+async def echo(websocket):
+    async for message in websocket:
+        ws.send(message)
+        msg = ws.recv()
+        print(msg)
+        await websocket.send(msg)
+
+async def main():
+    async with serve(echo, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
